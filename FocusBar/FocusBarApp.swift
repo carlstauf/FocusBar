8f97b1b0ea51e0f0a39f08ee6874d3a58d0535c5
Carl Stauffer <carlstauf@gmail.com>
2025-12-02 17:32:02 -0500

    Initial Commit

---
+++ FocusBar/FocusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBarApp.swift
+//  FocusBar
+//
+//  Created by Carl Stauffer on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
